99b8b57928d29a32d2cca68d829a70f7fcefce75
boominadhaprakash <boominadhaprakash@gmail.com>
2018-03-20 17:53:40 +0530

    Code cleanup

--- TouchID/ViewController.swift
+++ TouchID/ViewController.swift
@@ -11,45 +11,39 @@ import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    //MARK:- iVars
     @IBOutlet weak var enableaccessbtn: UIButton!
     
+    //MARK:- Overridden functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func enableaccessact(_ sender: Any) {
-        
+    //MARK:- Private functions
+    @IBAction private func enableaccessact(_ sender: Any) {
         let auth = LAContext()
         var error:NSError?
-        
-        if auth.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
-        {
-            //Authentication Success
-            print("Auth success")
+        if auth.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            debugPrint("Auth success")
             auth.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Touch ID", reply: {(success,error)in
-                if success{
-                    print("success")
-                }else{
-                    print("No Touch ID")
-                    
+                if success {
+                    debugPrint("success")
+                }else {
+                    debugPrint("No Touch ID")
                 }
             })
         }
-        else
-        {
+        else {
             let alertController = UIAlertController(title: "Error!", message: "Device doesn't support Biometric access/Touch ID", preferredStyle: .alert)
             let okAct = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(okAct)
-            
             self.present(alertController, animated: true, completion: nil)
         }
     }
-
 }
 
